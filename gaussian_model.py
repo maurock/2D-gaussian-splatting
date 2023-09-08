82e03d4f880e45058594593d01165dfefca2148a
ddrous[macos] <rd.nzoyemngueguin@bristol.ac.uk>
2023-09-08 20:55:13 +0100

    Setup iteration process in Jax

--- gaussian_model.py
+++ gaussian_model.py
@@ -1,32 +1,65 @@
 """
 This defines the 2D Gaussian model.
 """
-import numpy as np
-import torch
+import jax
+import jax.numpy as jnp
+from typing import NamedTuple
 
-class Gaussian2D:
+from utils import get_new_keys
+
+
+
+
+
+
+class Gaussian2D(NamedTuple):
     """Definition of a 2D Gaussian."""
-
-    def __init__(self, width=100, height=100):
-        self.mean = torch.tensor([np.random.uniform(0, width), np.random.uniform(0, height)], requires_grad=True, dtype=torch.float32)
-        self.scaling = torch.tensor(np.random.uniform(0, min(width, height) / 5, (2,1)), requires_grad=True, dtype=torch.float32)
-        #self.rotation = torch.tensor(np.random.uniform(0, 2 * np.pi), requires_grad=True, dtype=torch.float32)
-        self.rotation = torch.tensor(np.pi/4, requires_grad=True, dtype=torch.float32)
-        self.opacity = torch.tensor(np.random.uniform(0, 1), requires_grad=True, dtype=torch.float32)
-        self.colour = torch.tensor(np.random.uniform(0, 1, (1, 3)), requires_grad=True, dtype=torch.float32)
-
-    def get_covariance(self):
-        """Calculate the covariance matrix. """
-        scaling_matrix = torch.diag(self.scaling.squeeze())
-        rotation_matrix = torch.tensor([[torch.cos(self.rotation), -torch.sin(self.rotation)],
-                                    [torch.sin(self.rotation), torch.cos(self.rotation)]])
-        return rotation_matrix @ scaling_matrix @ scaling_matrix.T @ rotation_matrix.T  # TODO: turn scaling into a matrix?
-    
-    def get_density(self, x):
-        # TODO: add normalisation term if needed
-        return torch.exp(-0.5 *  (x - self.mean)[:, None].T  @ torch.linalg.inv(self.get_covariance()) @ (x - self.mean))
+    mean: jnp.ndarray
+    scaling: jnp.ndarray
+    rotation: jnp.ndarray
+    opacity: jnp.ndarray
+    colour: jnp.ndarray
 
 
+
+def init_gaussian(key, width=256., height=256.) -> Gaussian2D:
+    """Returns the initial model params."""
+    keys = get_new_keys(key, 5)
+
+    ## Uniformly initialise parameters of a 2D gaussian
+    mean = jax.random.uniform(keys[0], (2,1), minval=0, maxval=min(width, height))
+    scaling = jax.random.uniform(keys[1], (2,1), minval=0, maxval=min(width, height)/5)
+    rotation = jax.random.uniform(keys[2], (1,), minval=0, maxval=2*jnp.pi)
+    opacity = jax.random.uniform(keys[3], (1,), minval=0, maxval=1)
+    colour = jax.random.uniform(keys[4], (1,3), minval=0, maxval=1)
+
+    return Gaussian2D(mean, scaling, rotation, opacity, colour)
+
+
+def get_covariance(gaussian: Gaussian2D):
+    """Calculate the covariance matrix. """
+    scaling_matrix = jnp.diag(gaussian.scaling.squeeze())
+
+    cos, sin = jnp.cos(gaussian.rotation), jnp.sin(gaussian.rotation)
+    rotation_matrix = jnp.array([[cos, -sin], [sin, cos]]).squeeze()
+
+    # print("Covariance mat shape: ", (rotation_matrix @ scaling_matrix @ scaling_matrix.T @ rotation_matrix.T).shape)
+
+    return rotation_matrix @ scaling_matrix @ scaling_matrix.T @ rotation_matrix.T 
+
+
+def get_density(gaussian, x):
+    """Calculate the density of the gaussian at a given point."""
+    ret = jnp.exp(-0.5 * (x - gaussian.mean).T  @ jnp.linalg.inv(get_covariance(gaussian)) @ (x - gaussian.mean))
+
+    # print(" shapes: ", x.shape, gaussian.mean.shape,)
+    # print("Density shape: ", ((x - gaussian.mean).T  @ jnp.linalg.inv(get_covariance(gaussian)) @ (x - gaussian.mean)).shape)
+    # print("Density shape: ", ret.shape)
+
+    return ret.squeeze()
+
+
+    # return ret
 
 
 def is_positive_semi_definite(matrix):
@@ -35,7 +68,7 @@ def is_positive_semi_definite(matrix):
         return False
     
     # Compute eigenvalues
-    eigenvalues, _ = torch.eig(matrix, eigenvectors=False)
+    eigenvalues, _ = jnp.linalg.eig(matrix, eigenvectors=False)
     
     # Check if all eigenvalues are non-negative
     if (eigenvalues[:, 0] >= 0).all():
--- scene.py
+++ scene.py
@@ -1,18 +1,67 @@
+#%%
+
 """
 This defines a Scene, which is a collection of 2D Gaussians.
 """
-from gaussian_model import Gaussian2D
-import numpy as np
-import torch
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import optax
 
-class Scene:
+from typing import List, NamedTuple
+from gaussian_model import *
+from functools import partial
 
-    def __init__(self, image, N: int):
-        self.N = N
-        self.height, self.width = image.shape[:2]
-        self.gaussians = [Gaussian2D(self.width, self.height) for _ in range(N)]
 
-def render(scene: Scene):
+class Scene2D(NamedTuple):
+    """Definition of a 2D Scene."""
+    gaussians: List[Gaussian2D]
+    # width: int
+    # height: int
+
+
+def init_scene(key, image, N: int) -> Scene2D:
+    """Returns the initial model params."""
+    gaussians = [init_gaussian(key, image.shape[0], image.shape[1]) for _ in range(N)]
+    return Scene2D(gaussians)
+
+
+
+# @partial(jax.jit, static_argnums=(0,))
+def render_pixel(scene: Scene2D, x: jnp.ndarray):
+    """Render a single pixel."""
+    # densities = jnp.concatenate([get_density(gaussian, x) for gaussian in scene.gaussians], axis=0)
+
+    densities = jnp.array([get_density(gaussian, x) for gaussian in scene.gaussians])[:, None]
+    colours = jnp.concatenate([gaussian.colour for gaussian in scene.gaussians], axis=0)
+    opacities = jnp.array([gaussian.opacity for gaussian in scene.gaussians])
+
+    # ## Ugly trick to add extra dimensions to colours and opacities (For VMAPPING)
+    # col_len = len(colours.shape)
+    # extra_len = len(densities.shape) - col_len
+    # extra_dims = tuple([col_len+i for i in range(extra_len)])
+
+    # colours = jnp.expand_dims(colours, axis=tuple(extra_dims))
+    # opacities = jnp.expand_dims(opacities, axis=tuple(extra_dims))
+
+    return jnp.sum(densities * colours * opacities, axis=0)
+
+
+
+    # densities = [get_density(gaussian, x)[None, :] for gaussian in scene.gaussians]
+    # colours = [gaussian.colour for gaussian in scene.gaussians]
+    # opacities = [gaussian.opacity for gaussian in scene.gaussians]
+
+    # pytree = jax.tree_map(lambda d, c, o: d*c*o, densities, colours, opacities)
+    # return jnp.sum(pytree, axis=0)
+
+
+render_pixels_1D = jax.vmap(render_pixel, in_axes=(None, 0), out_axes=0)
+render_pixels_2D = jax.vmap(render_pixels_1D, in_axes=(None, 1), out_axes=1)
+
+
+
+def render(scene: Scene2D, ref_image: jnp.ndarray):
     """
     Process:
     1. Create a blank image.
@@ -20,18 +69,95 @@ def render(scene: Scene):
         2. Compute the footprint (value of each gaussian at that point)
         3. Multiply each footprint value by its RGB
         4. Average the resulting values
-    """ 
-    image = torch.zeros(size=(scene.width, scene.height, 3))
+    """
+    image = jnp.zeros_like(ref_image)
 
-    for i in range(0, image.shape[0]):
-        for j in range(0, image.shape[1]):
-            densities = torch.tensor([gaussian.get_density(torch.tensor([i, j], dtype=torch.float32)) for gaussian in scene.gaussians])
-            colours = torch.cat([gaussian.colour for gaussian in scene.gaussians], axis=0)
-            opacities = torch.tensor([gaussian.opacity for gaussian in scene.gaussians])
+    # for (int i = 0; i < N-1; i++){
+    #     for (int j = 0; j < M-1; j++){
+    #         l = i + j*(N-1);
 
-            image[i, j, :] = torch.sum(densities[:, None] * colours * opacities[:, None], axis=0)
-    
+    # for i in range(0, image.shape[0]):
+    #     for j in range(0, image.shape[1]):
+
+    #         x = jnp.array([i, j], dtype=jnp.float32)[:, None]
+
+    #         # densities = jnp.concatenate([get_density(gaussian, x) for gaussian in scene.gaussians], axis=0)
+    #         # colours = jnp.concatenate([gaussian.colour for gaussian in scene.gaussians], axis=0)
+    #         # opacities = jnp.array([gaussian.opacity for gaussian in scene.gaussians])
+
+    #         # image = image.at[i, j, :].set(jnp.sum(densities * colours * opacities, axis=0))
+
+    #         image = image.at[i, j, :].set(render_pixel(scene, x))
+
+
+
+    ## X is the meshgrids
+    meshgrid = jnp.meshgrid(jnp.arange(0, ref_image.shape[0]), jnp.arange(0, ref_image.shape[1]))
+    xs = jnp.stack(meshgrid, axis=0).T[..., None]
+
+    # print("xs shape: ", xs.shape)
+    image = render_pixels_2D(scene, xs)
+
+
     # image = image/image.max()
-    return image
+    return image.squeeze()
 
 
+# @jax.value_and_grad
+def mse_loss(scene: Scene2D, ref_image: jnp.ndarray):
+    """Calculate the MSE loss between the rendered image and the reference image."""
+    image = render(scene, ref_image)
+    ## Add penalty for values greater than 1
+    return jnp.mean((image - ref_image) ** 2) + 1.*jnp.sum(jnp.where(image >= 1., image, 0.))
+
+
+@partial(jax.jit, static_argnums=(3,))
+def train_step(scene: Scene2D, ref_image: jnp.ndarray, opt_state, optimiser):
+    """Perform a single training step."""
+    loss, grad = jax.value_and_grad(mse_loss)(scene, ref_image)
+    updates, new_opt_state = optimiser.update(grad, opt_state)
+    new_scene = optax.apply_updates(scene, updates)
+    return new_scene, new_opt_state, loss
+
+
+## Print the Scene2D as a pytree
+
+
+if __name__=='__main__':
+
+    # key = jax.random.PRNGKey(27)
+    key = None
+
+    scene = init_scene(key, jnp.zeros((256, 256)), 25)
+
+    # load image called luna.jpeg and save it as a numpy array
+    ref_image = plt.imread('luna.jpeg')/255.
+    plt.imshow(ref_image)
+    plt.show()
+
+    image = render(scene, ref_image)
+
+    plt.imshow(image)
+    plt.show()
+
+
+    ## Init optimiser
+    optimiser = optax.adam(1e-3)
+    ## Init optimiser state
+    opt_state = optimiser.init(scene)
+    ## Training loop
+    for i in range(5000):
+        scene, opt_state, loss = train_step(scene, ref_image, opt_state, optimiser)
+        # print("Loss: ", loss)
+        ## Print loss and iteration number
+        if i % 100 == 0 or i < 3:
+            print(f'Iteration: {i}            Loss: {loss:.3f}')
+
+
+    ## Evaluate the final scene
+    image = render(scene, ref_image)
+    plt.imshow(image)
+    plt.show()
+
+
+    print('Done')
